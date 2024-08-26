34a9b7fcab628ef9e3eeb85c61cd307a2c48b560
Mark Mwima <markmwimar@gmail.com>
2024-08-26 23:40:39 +0300

    springboot web intro

---
+++ src/main/java/com/mwimar/SpringBootWeb101/SpringBootWeb101Application.java
@@ -0,0 +1,13 @@
+package com.mwimar.SpringBootWeb101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWeb101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWeb101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mwimar/SpringBootWeb101/SpringBootWeb101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwimar.SpringBootWeb101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWeb101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
