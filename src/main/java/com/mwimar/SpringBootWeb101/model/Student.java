827cef1c011b1f0a5f049761736ffa4108ccabab
Mark Mwima <markmwimar@gmail.com>
2024-08-27 23:38:38 +0300

    getters and setters

--- src/main/java/com/mwimar/SpringBootWeb101/model/Student.java
+++ src/main/java/com/mwimar/SpringBootWeb101/model/Student.java
@@ -11,4 +11,20 @@ public class Student {
         this.age = age;
         this.name = name;
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
