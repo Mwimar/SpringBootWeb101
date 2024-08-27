f28d8f771dff58fc5d061822dd32dd10579f58cd
Mark Mwima <markmwimar@gmail.com>
2024-08-27 23:42:40 +0300

    student bean

--- src/main/java/com/mwimar/SpringBootWeb101/SpringBootWeb101Application.java
+++ src/main/java/com/mwimar/SpringBootWeb101/SpringBootWeb101Application.java
@@ -1,5 +1,6 @@
 package com.mwimar.SpringBootWeb101;
 
+import com.mwimar.SpringBootWeb101.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,6 +10,7 @@ public class SpringBootWeb101Application {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringBootWeb101Application.class, args);
+		Student student = context.getBean(Student.class);
 	}
 
 }
