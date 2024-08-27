c3970edf53499f7e5ea13d171dccaf9122442cb3
Mark Mwima <markmwimar@gmail.com>
2024-08-27 23:40:16 +0300

    application context

--- src/main/java/com/mwimar/SpringBootWeb101/SpringBootWeb101Application.java
+++ src/main/java/com/mwimar/SpringBootWeb101/SpringBootWeb101Application.java
@@ -2,12 +2,13 @@ package com.mwimar.SpringBootWeb101;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootWeb101Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWeb101Application.class, args);
+		ApplicationContext context = SpringApplication.run(SpringBootWeb101Application.class, args);
 	}
 
 }
