30a7206574371b1cf141c9920cbb59e42441bf0a
Mark Mwima <markmwimar@gmail.com>
2024-08-27 23:39:16 +0300

    to string

--- src/main/java/com/mwimar/SpringBootWeb101/model/Student.java
+++ src/main/java/com/mwimar/SpringBootWeb101/model/Student.java
@@ -27,4 +27,12 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
