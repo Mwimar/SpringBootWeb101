4672b6ec12904dbb34363b5082bf3f9ee7276ee6
Mark Mwima <markmwimar@gmail.com>
2024-08-30 15:44:25 +0300

    deleted student and dorm classes

--- src/main/java/com/mwimar/SpringBootWeb101/SpringBootWeb101Application.java
+++ src/main/java/com/mwimar/SpringBootWeb101/SpringBootWeb101Application.java
@@ -1,8 +1,5 @@
 package com.mwimar.SpringBootWeb101;
 
-import com.mwimar.SpringBootWeb101.model.Dorm;
-import com.mwimar.SpringBootWeb101.model.Student;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,12 +9,10 @@ public class SpringBootWeb101Application {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringBootWeb101Application.class, args);
-		Student student = context.getBean(Student.class);
-		Dorm dorm = context.getBean(Dorm.class);
 
 
 		
-		student.register(dorm );
+
 	}
 
 }
--- src/main/java/com/mwimar/SpringBootWeb101/model/Dorm.java
+++
@@ -1,7 +0,0 @@
-package com.mwimar.SpringBootWeb101.model;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class Dorm {
-}
