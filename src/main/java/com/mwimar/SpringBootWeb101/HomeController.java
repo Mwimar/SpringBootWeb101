5e5aa17425a141f4b2c712c4710912075d3d1b35
Mark Mwima <markmwimar@gmail.com>
2024-09-01 14:57:32 +0300

    getting results from user

--- src/main/java/com/mwimar/SpringBootWeb101/HomeController.java
+++ src/main/java/com/mwimar/SpringBootWeb101/HomeController.java
@@ -1,6 +1,7 @@
 package com.mwimar.SpringBootWeb101;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,8 +15,11 @@ public class HomeController {
     };
 
     @RequestMapping("/add")
-    public String result(){
-        System.out.println("in add");
+    public String result(HttpServletRequest req){
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int result= num1 + num2;
+        System.out.println(result);
         return"result.jsp";
     }
 
