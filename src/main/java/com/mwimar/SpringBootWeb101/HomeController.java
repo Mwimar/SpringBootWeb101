ad21c8ad40b53d2734b9eafea95ec5dbc4593f2a
Mark Mwima <markmwimar@gmail.com>
2024-09-02 14:08:26 +0300

    model and view

--- src/main/java/com/mwimar/SpringBootWeb101/HomeController.java
+++ src/main/java/com/mwimar/SpringBootWeb101/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -18,13 +19,14 @@ public class HomeController {
     };
 
     @RequestMapping("/add")
-    public String result(@RequestParam("num1") int num, @RequestParam("num2")int num2, Model model){
+    public ModelAndView result(@RequestParam("num1") int num, @RequestParam("num2")int num2, ModelAndView mv){
 
         int result= num + num2;
-        model.addAttribute("result", result);
+        mv.addObject("result", result);
+//        model.addAttribute("result", result);
 
         System.out.println(result);
-        return"result";
+        return mv;
     }
 
 }
