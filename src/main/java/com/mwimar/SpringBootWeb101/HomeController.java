e5e861367aa65813260139095f719a24f6d7988a
Mark Mwima <markmwimar@gmail.com>
2024-09-02 15:18:22 +0300

    Model attribute

--- src/main/java/com/mwimar/SpringBootWeb101/HomeController.java
+++ src/main/java/com/mwimar/SpringBootWeb101/HomeController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,18 +32,17 @@ public class HomeController {
     }
 
     @RequestMapping("/addAlien")
-    public ModelAndView addAlien(@RequestParam("aid") int aid, @RequestParam("aname")String  aname, ModelAndView mv){
+    public String addAlien(@ModelAttribute Alien alien){
 
-        Alien alien = new Alien();
-        alien.setAid(aid);
-        alien.setAname(aname);
-
-        mv.addObject("alien", alien);
+//        Alien alien = new Alien();
+//        alien.setAid(aid);
+//        alien.setAname(aname);
+//
+//        mv.addObject("alien", alien);
 //        model.addAttribute("result", result);
-        mv.setViewName("result");
+//        mv.setViewName("result");
 
-        System.out.println();
-        return mv;
+          return "result";
     }
 
 }
