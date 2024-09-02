0712047c10b226650d0de5666cec49a3eda2cddb
Mark Mwima <markmwimar@gmail.com>
2024-09-02 15:08:47 +0300

    setting alien object values

--- src/main/java/com/mwimar/SpringBootWeb101/HomeController.java
+++ src/main/java/com/mwimar/SpringBootWeb101/HomeController.java
@@ -34,6 +34,8 @@ public class HomeController {
     public ModelAndView addAlien(@RequestParam("aid") int aid, @RequestParam("aname")String  aname, ModelAndView mv){
 
         Alien alien = new Alien();
+        alien.setAid(aid);
+        alien.setAname(aname);
 
         mv.addObject("alien", alien);
 //        model.addAttribute("result", result);
